7cc1f3353aabb747f4f4be8579a38c69a901b7e5
Dev Bilaspure <devbilaspure4941@gmail.com>
2023-03-29 13:41:14 +0530

    added comments in app.py

--- spam-sms-classifier-application/venv/app.py
+++ spam-sms-classifier-application/venv/app.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import streamlit as st
 import pickle
 import string
@@ -5,14 +6,18 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+# Initialize a Porter Stemmer instance
 ps = PorterStemmer()
 
-
+# Define a function for text preprocessing
 def transform_text(text):
+    # Convert text to lowercase
     text = text.lower()
+    # Tokenize the text
     text = nltk.word_tokenize(text)
 
     y = []
+    # Remove non-alphanumeric characters
     for i in text:
         if i.isalnum():
             y.append(i)
@@ -20,6 +25,7 @@ def transform_text(text):
     text = y[:]
     y.clear()
 
+    # Remove stopwords and punctuation
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
@@ -27,27 +33,32 @@ def transform_text(text):
     text = y[:]
     y.clear()
 
+    # Apply stemming
     for i in text:
         y.append(ps.stem(i))
 
+    # Return the preprocessed text
     return " ".join(y)
 
+# Load the saved TF-IDF vectorizer and trained model
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
+# Create a Streamlit application titled "Spam Message Classifier"
 st.title("Spam Message Classifier")
 
+# Add a text area for user input
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
 
-    # 1. preprocess
+    # Preprocess the input message
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    # Vectorize the preprocessed message using the saved vectorizer
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    # Use the trained model to predict the label of the message
     result = model.predict(vector_input)[0]
-    # 4. Display
+    # Display the predicted label
     if result == 1:
         st.header("Spam")
     else:
